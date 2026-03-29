dd8166b321607703f3ef6879274ed0bd3eef7e26
aiyanami <964427336@qq.com>
2026-03-29 07:18:11 +0000

    feat:skill-fix

--- build/types/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// Type definitions for AutoResearch plugin
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RefinementOutcome = void 0;
-var RefinementOutcome;
-(function (RefinementOutcome) {
-    RefinementOutcome["APPROVED"] = "approved";
-    RefinementOutcome["REJECTED"] = "rejected";
-})(RefinementOutcome || (exports.RefinementOutcome = RefinementOutcome = {}));
